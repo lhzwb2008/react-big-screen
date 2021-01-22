a2ab83e6afa0c2bded18ed25d5952c3dc768d0e7
清穹 <zark.zhangwb@alibaba-inc.com>
2021-01-22 20:17:15 +0800

    add 4floor

--- src/components/rightPage/charts/options.js
+++ src/components/rightPage/charts/options.js
@@ -2,83 +2,129 @@ import echarts from 'echarts/lib/echarts';
 
 // 关联数据类别
 export const BrowseCategoriesOptions = params => ({
-  radar: {
-    center: ['50%', '50%'],
-    radius: '70%',
-    name: {
-      formatter: function (name) {
-        let arr;
-        arr = ['{a|' + name + '}'];
-        return arr.join('\n');
-      },
-      textStyle: {
-        rich: {
-          //根据文字的组设置格式
-          a: {
-            color: '#BCDCFF',
-            fontSize: 14,
-            fontWeight: 600,
-            fontFamily: 'Source Han Sans CN',
-          },
-        },
-      },
-    },
-    // 名字和图形的距离
-    nameGap: 5,
-    indicator: params.indicator,
-    splitLine: {
-      show: false,
-    },
-    axisLine: {
-      show: false,
-    },
-    splitArea: {
-      areaStyle: {
-        color: [
-          'rgba(84,136,255, 0.05)',
-          'rgba(84,136,255, 0.1)',
-          'rgba(84,136,255, 0.2)',
-          'rgba(84,136,255, 0.3)',
-          'rgba(84,136,255, 0.4)',
-          'rgba(84,136,255, 0.5)',
-        ].reverse(),
-        shadowColor: 'rgba(0, 0, 0, .5)',
-        shadowBlur: 5,
-        shadowOffsetX: 10,
-        shadowOffsetY: 10,
-      },
-    },
-  },
+
   series: [
     {
-      name: '用户浏览类别',
-      type: 'radar',
-      data: [params.data],
+      name: '四层楼',
+      type: 'funnel',
+      min: 0,
+      max: 100,
+      left: 20,
+      top: 0,
+      width: '80%',
+      height: '100%',
+      minSize: '0%',
+      maxSize: '100%',
+      sort: 'ascending',
+      gap: 0,
+      itemStyle: {
+        borderColor: '#fff',
+        borderWidth: 1
+      },
       label: {
-        show: false,
-        formatter: function (params) {
-          return params.value + '万';
-        },
-        color: '#9ae8ac',
-        distance: 10,
-        align: 'right',
+        show: true,
+        position: 'inside'
       },
-      symbol: 'none',
-      symbolSize: [6, 6],
-      // 边缘颜色
-      lineStyle: {
-        color: 'rgba(160,159,246, 0.6)',
-        width: 2,
+      labelLine: {
+        length: 10,
+        lineStyle: {
+          width: 1,
+          type: 'solid'
+        }
       },
-      areaStyle: {
-        color: 'rgba(114,113,233,.4)',
-        opacity: 0.8,
-        shadowColor: 'rgba(115,149,255,1)',
-        shadowBlur: 10,
+      emphasis: {
+        label: {
+          fontSize: 20
+        }
       },
-    },
-  ],
+      data: [
+        { value: 25, name: '组织发展云' },
+        { value: 50, name: '人才管理云' },
+        { value: 75, name: '招聘管理云' },
+        { value: 100, name: '人事管理云' }
+      ]
+    }
+  ]
 });
+
+// export const BrowseCategoriesOptions = params => ({
+//   radar: {
+//     center: ['50%', '50%'],
+//     radius: '70%',
+//     name: {
+//       formatter: function (name) {
+//         let arr;
+//         arr = ['{a|' + name + '}'];
+//         return arr.join('\n');
+//       },
+//       textStyle: {
+//         rich: {
+//           //根据文字的组设置格式
+//           a: {
+//             color: '#BCDCFF',
+//             fontSize: 14,
+//             fontWeight: 600,
+//             fontFamily: 'Source Han Sans CN',
+//           },
+//         },
+//       },
+//     },
+//     // 名字和图形的距离
+//     nameGap: 5,
+//     indicator: params.indicator,
+//     splitLine: {
+//       show: false,
+//     },
+//     axisLine: {
+//       show: false,
+//     },
+//     splitArea: {
+//       areaStyle: {
+//         color: [
+//           'rgba(84,136,255, 0.05)',
+//           'rgba(84,136,255, 0.1)',
+//           'rgba(84,136,255, 0.2)',
+//           'rgba(84,136,255, 0.3)',
+//           'rgba(84,136,255, 0.4)',
+//           'rgba(84,136,255, 0.5)',
+//         ].reverse(),
+//         shadowColor: 'rgba(0, 0, 0, .5)',
+//         shadowBlur: 5,
+//         shadowOffsetX: 10,
+//         shadowOffsetY: 10,
+//       },
+//     },
+//   },
+//   series: [
+//     {
+//       name: '用户浏览类别',
+//       type: 'radar',
+//       data: [params.data],
+//       label: {
+//         show: false,
+//         formatter: function (params) {
+//           return params.value + '万';
+//         },
+//         color: '#9ae8ac',
+//         distance: 10,
+//         align: 'right',
+//       },
+//       symbol: 'none',
+//       symbolSize: [6, 6],
+//       // 边缘颜色
+//       lineStyle: {
+//         color: 'rgba(160,159,246, 0.6)',
+//         width: 2,
+//       },
+//       areaStyle: {
+//         color: 'rgba(114,113,233,.4)',
+//         opacity: 0.8,
+//         shadowColor: 'rgba(115,149,255,1)',
+//         shadowBlur: 10,
+//       },
+//     },
+//   ],
+// });
 
 // 反馈
 export const FeedbackOptions = params => ({
